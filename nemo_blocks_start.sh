2a3caeb7fbaedf8c6a33e53985b213e21f88a8ed
zachlopez <zlopez@princeton.edu>
2021-06-23 17:32:16 +0800

    Added server start files

---
+++ nemo_blocks_start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PID_TRES=$(lsof -t -i :3000)
+while [ ! -z "$PID_TRES" ]
+do
+echo "Process $PID_TRES killed."
+kill -9 $PID_TRES
+PID_TRES=$(lsof -t -i :3000)
+done
+
+PID_FIVE=$(lsof -t -i :5000)
+while [ ! -z "$PID_FIVE" ]
+do
+echo "Process $PID_FIVE killed."
+kill -9 $PID_FIVE
+PID_FIVE=$(lsof -t -i :5000)
+done
+
+rm -rf node_modules
+npm install
+yarn dev
