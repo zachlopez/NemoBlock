fe7ed4a22887de1c600fe976522ae5bd20d383b9
zachlopez <zlopez@princeton.edu>
2021-07-11 21:45:53 +0800

    Changed setup bash to make backup database

--- nemo_blocks_setup.sh
+++ nemo_blocks_setup.sh
@@ -6,6 +6,7 @@ npm install
 
 sudo chmod +x /home/zach/NemoBlock/nemo_blocks_start.sh
 sudo cp nemo_blocks.service /lib/systemd/system/nemo_blocks.service
+sudo cp /home/zach/NemoBlock/nemo.db /home/zach/nemo-backup.db
 sudo systemctl daemon-reload
 sudo systemctl disable nemo_blocks.service
 sudo systemctl enable nemo_blocks.service
